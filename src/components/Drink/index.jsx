3d8c4e2501c6a9760341f6273d3939ed3655dd05
Michaela Heidlerová <michaela.heidlerova@gmail.com>
2025-05-03 13:46:58 +0200

    Přidán nápoj jako komponenta

---
+++ src/components/Drink/index.jsx
@@ -0,0 +1,20 @@
+import './style.css';
+
+export const Drink = ({ name, image }) => {
+  return (
+    <div className="drink">
+      <div className="drink__product">
+        <div className="drink__cup">
+          <img src={image} alt={name} />
+        </div>
+        <div className="drink__info">
+          <h3>{name}</h3>
+          {/* Ingredience (layers) doplníme později */}
+        </div>
+      </div>
+      <form className="drink__controls">
+        <button className="order-btn">Objednat</button>
+      </form>
+    </div>
+  );
+};
